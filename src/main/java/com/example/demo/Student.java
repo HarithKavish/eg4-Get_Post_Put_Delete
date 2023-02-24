44f1adaf68f0fa5c7df77b8460dec970f3c10a5b
HarithKavish <harithkavish40@gmail.com>
2023-02-24 15:55:19 +0530

    commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	private int id;
+	private String name;
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNameString() {
+		return name;
+	}
+	public void setNameString(String name) {
+		this.name = name;
+	}
+}
